7079743cca7a29fd63f40db37676beb73bc0e2e5
Godspower <gpanyaog@gmail.com>
2019-12-28 10:36:20 +0100

    cap

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDlMYlj_SP64IOPQWRYfBrPo1FN6Pf0nMw",
-    authDomain: "cloud-b7334.firebaseapp.com",
-    databaseURL: "https://cloud-b7334.firebaseio.com",
-    projectId: "cloud-b7334",
-    storageBucket: "cloud-b7334.appspot.com",
-    messagingSenderId: "1042706051253",
-    appId: "1:1042706051253:web:6c0c622401e2f0644d0efc",
-    measurementId: "G-706RGTWYSG"
+    apiKey: "AIzaSyDinqTnPJGlK3atOfjoXPQvgz-R1pvnO2M",
+    authDomain: "capital-4adfd.firebaseapp.com",
+    databaseURL: "https://capital-4adfd.firebaseio.com",
+    projectId: "capital-4adfd",
+    storageBucket: "capital-4adfd.appspot.com",
+    messagingSenderId: "605300834564",
+    appId: "1:605300834564:web:4a419ce45bfa512a805051"
 }
 
 const app = firebase.initializeApp(config);
